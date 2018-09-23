b6a5415c07b55f84387930b267a6a5be6ba3dcac
icantwritecode <andrchas@gmail.com>
2018-09-23 20:50:55 +0300

    added /produts & /orders routes

---
+++ api/routes/products.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/', (req, res, next) => {
+    res.status(200).json({
+        message: 'Handling get requests to /products'
+    });
+});
+
+router.post('/', (req, res, next) => {
+    res.status(201).json({
+        message: 'Handling post requests to /products'
+    })
+});
+
+router.get('/:productId', (req, res, next) => {
+    const id = req.params.productId;
+    if (id === 'special') {
+        res.status(200).json({
+            message: 'You discovered a special route',
+            id: id
+        });
+    } else {
+        res.status(200).json({
+            message: 'You passed an ID',
+            id: id
+        });
+    }
+});
+
+router.patch('/:productId', (req, res, next) => {
+    res.status(200).json({
+        message: 'Updated Product'
+    });
+});
+
+router.delete('/:productId', (req, res, next) => {
+    res.status(200).json({
+        message: 'Deleted Product'
+    });
+});
+
+module.exports = router;
--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const app = express();
 
+// Import Of Routes
+const productsRoutes = require('./api/routes/products');
+const ordersRoutes = require('./api/routes/orders');
+
+
 // .use is called midleware
-app.use((req, res, next) => {
-    res.status(200).json({
-        message: 'It Works!'
-    });
-});
+app.use('/products', productsRoutes);
+app.use('/orders', ordersRoutes);
 
 module.exports = app;
