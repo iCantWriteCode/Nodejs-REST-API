18650e7bd50c41ae8af615059a9ff1c2124d5dc6
icantwritecode <andrchas@gmail.com>
2018-09-23 21:26:41 +0300

    added post methods for products and orders & midlewares

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # Nodejs-REST-API
+
+# Products Route 
+
+## Post a product
+name
+price
+
+# Order Route 
+
+## Post a order
+productId
+quantity
--- app.js
+++ app.js
@@ -1,13 +1,28 @@
 const express = require('express');
 const app = express();
 const morgan = require('morgan');
+const bodyParser = require('body-parser');
 
 // Routes Files
 const productsRoutes = require('./api/routes/products');
 const ordersRoutes = require('./api/routes/orders');
 
-// Morgan is a logger middleware
+// Middlewares
 app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+
+    if (req.method === 'OPTIONS') {
+        res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+        return res.status(200).json()
+    }
+    next();
+});
 
 // Route
 app.use('/products', productsRoutes);
@@ -22,6 +37,7 @@ app.use((req, res, next) => {
     next(error);
 });
 
+// Other Errors
 app.use((error, req, res, next) => {
     res.status(error.status || 500);
     res.json({
