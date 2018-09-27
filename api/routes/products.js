cc749d0d043ed1dfb753a211bf865ade8ee8597c
icantwritecode <andrchas@gmail.com>
2018-09-27 22:36:56 +0300

    All product routes are now executing on database

--- api/routes/products.js
+++ api/routes/products.js
@@ -5,9 +5,16 @@ const mongoose = require('mongoose');
 const Product = require('../models/product')
 
 router.get('/', (req, res, next) => {
-    res.status(200).json({
-        message: 'Handling get requests to /products'
-    });
+    Product.find()
+        .exec()
+        .then(docs => {
+            console.log(docs)
+            res.status(200).json(docs)
+        })
+        .catch(err => {
+            console.log(err)
+            res.status(500).json(err)
+        })
 });
 
 router.post('/', (req, res, next) => {
@@ -32,7 +39,6 @@ router.post('/', (req, res, next) => {
             })
         });
 
-
 });
 
 router.get('/:productId', (req, res, next) => {
@@ -55,15 +61,39 @@ router.get('/:productId', (req, res, next) => {
 });
 
 router.patch('/:productId', (req, res, next) => {
-    res.status(200).json({
-        message: 'Updated Product'
-    });
+    const id = req.params.productId;
+    const updateOps = {}
+    for (const ops of req.body) {
+        updateOps[ops.propName] = ops.value;
+    }
+    Product.update(
+        { _id: id },
+        { $set: updateOps }
+    )
+        .exec()
+        .then(result => {
+            console.log(result)
+            res.status(200).json(result)
+        })
+        .catch(err => {
+            console.log(err)
+            res.status(500).json(err)
+        })
 });
 
 router.delete('/:productId', (req, res, next) => {
-    res.status(200).json({
-        message: 'Deleted Product'
-    });
+    const id = req.params.productId;
+    Product.remove({ _id: id })
+        .exec()
+        .then(result => {
+            res.status(200).json(result)
+        })
+        .catch(err => {
+            console.log(err)
+            res.status(500).json({
+                error: err
+            })
+        })
 });
 
 module.exports = router;
